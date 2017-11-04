535d0f342d93a08d9762f0a562154db73cb50969
Manoj Vivek <p.manoj.vivek@gmail.com>
2017-11-04 14:30:34 +0530

    Added new freeze strategies

--- src/grid-freezer.js
+++ src/grid-freezer.js
@@ -1,7 +1,14 @@
 'use strict';
 const {new2DArray, randomizeArray} = require('./util.js');
 
-const freezeStrategies = [cornerOnly, boundariesOnly];
+const freezeStrategies = [
+  boundariesOnly,
+  cornerOnly,
+  evenCells,
+  evenOddAlternative,
+  leftAndRight,
+  topAndBottom,
+];
 
 function cornerOnly(y, x, height, width) {
   ////////////////////
@@ -13,10 +20,10 @@ function cornerOnly(y, x, height, width) {
   //                //
   //*              *//
   ////////////////////
-  return (x == 0 && y == 0) ||
-    (x == 0 && y == height - 1) ||
-    (x == width - 1 && y == 0) ||
-    (x == width - 1 && y == height - 1);
+  return (x === 0 && y === 0) ||
+    (x === 0 && y === (height - 1)) ||
+    (x === (width - 1) && y === 0) ||
+    (x === (width - 1) && y === (height - 1));
 }
 
 function boundariesOnly(y, x, height, width) {
@@ -29,7 +36,59 @@ function boundariesOnly(y, x, height, width) {
   //*              *//
   //****************//
   ////////////////////
-  return x == 0 || y == 0 || x == width -1 || y == height -1;
+  return x === 0 || y === 0 || x === (width - 1) || y === (height - 1);
+}
+
+function topAndBottom(y, x, height, width) {
+  ////////////////////
+  //****************//
+  //                //
+  //                //
+  //                //
+  //                //
+  //                //
+  //****************//
+  ////////////////////
+  return y === 0 || y === (height -1);
+}
+
+function leftAndRight(y, x, height, width) {
+  ////////////////////
+  //*              *//
+  //*              *//
+  //*              *//
+  //*              *//
+  //*              *//
+  //*              *//
+  //*              *//
+  ////////////////////
+  return x === 0 || x === (width - 1);
+}
+
+function evenCells(y, x, height, width) {
+  ///////////////////
+  //* * * * * * * *//
+  //               //
+  //* * * * * * * *//
+  //               //
+  //* * * * * * * *//
+  //               //
+  //* * * * * * * *//
+  ///////////////////
+  return (x % 2) == 0 && (y % 2) == 0;
+}
+
+function evenOddAlternative(y, x, height, width) {
+  ///////////////////
+  //* * * * * * * *//
+  // * * * * * * * //
+  //* * * * * * * *//
+  // * * * * * * * //
+  //* * * * * * * *//
+  // * * * * * * * //
+  //* * * * * * * *//
+  ///////////////////
+  return (x % 2) == (y % 2);
 }
 
 function freeze(grid) {
